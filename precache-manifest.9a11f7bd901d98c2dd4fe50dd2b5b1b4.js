bd9535148834a233d4f29e22d3ae902c54a66274
23subbhashit <43717493+23subbhashit@users.noreply.github.com>
2020-02-11 17:44:53 +0530

    Updates

---
+++ precache-manifest.9a11f7bd901d98c2dd4fe50dd2b5b1b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b55355d6e39e083df5736e75c21f521",
+    "url": "/Meme-Generator/index.html"
+  },
+  {
+    "revision": "a610dbba11bc67c16ec5",
+    "url": "/Meme-Generator/static/css/main.fd61556d.chunk.css"
+  },
+  {
+    "revision": "d74ed7bfcc822e0c4e2d",
+    "url": "/Meme-Generator/static/js/2.5d158d0e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Meme-Generator/static/js/2.5d158d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a610dbba11bc67c16ec5",
+    "url": "/Meme-Generator/static/js/main.6c3b064f.chunk.js"
+  },
+  {
+    "revision": "f9476099d80f9d48e41d",
+    "url": "/Meme-Generator/static/js/runtime-main.f91abc57.js"
+  }
+]);
